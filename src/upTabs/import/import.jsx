84eb429b96b0eeb791b08100b19a0d01c7e0856d
Даниил <daniil@MacBook-Pro-konstantin-2.local>
2023-12-08 15:49:11 +0300

    delete button in input

--- src/upTabs/import/import.jsx
+++ src/upTabs/import/import.jsx
@@ -1,35 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import { Modal, Button } from "react-bootstrap";
 
 const Import = () => {
+  const [modalIsOpen, setModalIsOpen] = useState({ show: false, data: { quantityAdd: 0, quantityMissing: 0 }});
+  const [disableButton, setDisabelButton] = useState(false);
+
+  const openModal = (data) => {
+    setModalIsOpen({ show: true, data });
+  };
+
+  const closeModal = () => {
+    setModalIsOpen({ ...modalIsOpen, show: false });
+  };
 
   const setFile = (e) => {
     e.preventDefault();
+  
+    const file = e.target.files[0]; 
 
-    const file = e.target.querySelector('#formFile').files[0]; 
+    if (!file) return;
+
     const regexpTestForName = /xls(x)?$/;
     const reader = new FileReader();
-
+  
     reader.onload = () => {
       const { name } = file;
       if (regexpTestForName.test(name)) {
         const data = reader.result.split(',')[1];
-
         const addFile = async () => await axios.post('http://localhost:8080/addFile', { file: data })
-          .then(({ data }) => console.log(data));
+          .then(({ data }) => openModal(data));
         addFile();
       }
     };
-
+  
     reader.readAsDataURL(file);
   };
 
+  console.log(modalIsOpen);
+
   return (
-    <form className="mb-3" onSubmit={setFile}>
-      <label htmlFor="formFile" className="form-label">Поддерживаемые брокерские отчеты: Тинькофф, ВТБ</label>
-      <input className="form-control" type="file" id="formFile"/>
-      <button type="submit">Загрузить</button>
-    </form>
+    <>
+      <form className="mb-3" onChange={setFile}>
+        <label htmlFor="formFile" className="form-label">Поддерживаемые брокерские отчеты: Тинькофф, ВТБ</label>
+        <input className="form-control" type="file" id="formFile"/>
+      </form>
+      <Modal show={modalIsOpen.show} onHide={closeModal}>
+        <Modal.Body>Добавлено транзакций: {modalIsOpen.data.quantityAdd}</Modal.Body>
+        <Modal.Body>Не добавлено транзакций (дублирование): {modalIsOpen.data.quantityMissing}</Modal.Body>
+      </Modal>
+    </>
   )
 };
 
