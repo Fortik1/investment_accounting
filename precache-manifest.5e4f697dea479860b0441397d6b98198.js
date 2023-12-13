c4678d77c1dc6041907efeda600b02c890d00325
Даниил <daniil@MacBook-Pro-konstantin-2.local>
2023-12-13 18:23:20 +0300

    Updates

---
+++ precache-manifest.5e4f697dea479860b0441397d6b98198.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc9c86c16a26ca25f07b0db0132a9341",
+    "url": "/investment_accounting/index.html"
+  },
+  {
+    "revision": "d86a9d5acabb76250a46",
+    "url": "/investment_accounting/static/css/2.75bc9036.chunk.css"
+  },
+  {
+    "revision": "4e027d4c685e91673c4b",
+    "url": "/investment_accounting/static/css/main.73a48595.chunk.css"
+  },
+  {
+    "revision": "d86a9d5acabb76250a46",
+    "url": "/investment_accounting/static/js/2.87155011.chunk.js"
+  },
+  {
+    "revision": "4e027d4c685e91673c4b",
+    "url": "/investment_accounting/static/js/main.3889155c.chunk.js"
+  },
+  {
+    "revision": "ca07c6f29de0ddcc1910",
+    "url": "/investment_accounting/static/js/runtime~main.ea7fa268.js"
+  }
+]);
