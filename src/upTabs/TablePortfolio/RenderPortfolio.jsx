2faf3a3b780dbc2b8e55ffcae77ee947067047d4
Даниил <daniil@MacBook-Pro-konstantin-2.local>
2023-12-15 14:41:00 +0300

    edit RenderPortfolio

--- src/upTabs/TablePortfolio/RenderPortfolio.jsx
+++ src/upTabs/TablePortfolio/RenderPortfolio.jsx
@@ -1,14 +1,34 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { uniqueId } from "lodash";
+import { pick, uniqueId } from "lodash";
 
-const RenderTags = ({ tags }) => {
+const widthStyle = {
+  "accruedCouponEod": 150,
+  "ccy": 50,
+  "count": 100,
+  "couponPaymentFrequency": 170,
+  "dateDaily": 100,
+  "duration": 100,
+  "isin": 150,
+  "maturityDate": 100,
+  "name": 100,
+  "priceAvg": 50,
+  "priceDaily": 50,
+  "rating": 50,
+  "type": 50,
+  "yieldAvg": 50,
+  "yieldDaily": 50,
+};
+
+// accruedCouponEod (нужно умножить на count) он в % -> ((accruedCouponEod / 100) * principal) * count <- сумма нкд
+
+const RenderTags = ({ tags, length }) => {
   return (
     <>
-      <div className="portfolio-tags">
-        {Object.keys(tags).map((key, index) => 
-          <div key={uniqueId()} className={`col-${index}`}>
-            {key}
+      <div className="portfolio-tags" style={{ width: length + "px"}}>
+        {tags.map(({ oldName, newName }, index) =>
+          <div key={uniqueId()} style={{ width: widthStyle[oldName] + "px"}} >
+            {newName}
           </div>
         )}
       </div>
@@ -16,13 +36,13 @@ const RenderTags = ({ tags }) => {
   )
 };
 
-const RenderBody = ({ body }) => {
+const RenderBody = ({ body, length }) => {
   return (
     <>
-      {body.map((element) => 
-        <div className="portfolio-body" key={uniqueId()}>
-          {Object.entries(element).map(([key, value], index) => 
-            <div key={uniqueId()} className={`col-${index}`}>{value}</div>
+      {body.map((element) =>
+        <div className="portfolio-body" key={uniqueId()} style={{ width: length + "px" }}>
+          {Object.entries(element).map(([key, value], index) =>
+            <div key={uniqueId()} style={{ width: widthStyle[key] + "px" }}>{value}</div>
           )}
         </div>
       )}
@@ -30,24 +50,64 @@ const RenderBody = ({ body }) => {
   )
 };
 
+const principal = 1000; 
+
+const filterData = (data, currentNames) => data.map((element) => {
+  const newData = pick(element, currentNames);
+  const nkdName = 'accruedCouponEod';
+  newData[nkdName] = element[nkdName] / 100 * principal * element.count || null;
+  return newData;
+});
+
+const filterTags = (tags, correctName) => Object.keys(tags)
+  .reduce((acc, oldName) => acc = [...acc, { oldName, newName: correctName[oldName] }] ,[]);
+
 const RenderPortfolio = () => {
-  const [reqData, setReqData] = useState('');
+  const defaultName = ['name', 'priceAvg', 'priceDaily', 'yieldAvg', 'yieldDaily', 'couponPaymentFrequency', 'type', 'accruedCouponEod', 'count'];
+
+  const [reqData, setReqData] = useState({ dateDaily: '-' });
+  const [currentNames, setCurrentNames] = useState(defaultName);
+
+  const correctName = {
+    'name': "Name", 
+    'priceAvg': "Price (avg)",
+    'priceDaily': "Price Daily", 
+    'yieldAvg': "Yield Avg", 
+    'yieldDaily': `Yield ${reqData.dateDaily}`,
+    'couponPaymentFrequency': "Coupons",
+    'type': "Type",
+    'accruedCouponEod': "NKD", // по формуле
+    'count': "Count", // выводить досюда 
+    'rating': "Rating",
+    'ccy': "Ccy",
+    'dateDaily': "Daily Date",
+    'duration': "Duration",
+    'isin': "ISIN",
+    'maturityDate': "Maturity Date",
+  };
 
   useEffect(() => {
     const path = 'https://d5dpil1j3vqslj3529om.apigw.yandexcloud.net/api/v1/portfolio';
 
-    const getPortfolio = async () => await axios.get(path)
-      .then(({ data }) => setReqData(data));
+    (function async () { 
+      axios.get(path)
+        .then(({ data }) => setReqData({ 
+          ...reqData, 
+          data: filterData(data, currentNames),
+          dateDaily: data[0].dateDaily 
+        }))
+    }());
 
-    getPortfolio();
-  }, [reqData.limit, reqData.page]);
+  }, []);
+
+  const getLength = reqData.data && Object.keys(reqData.data[0]).reduce((acc, e) => acc + widthStyle[e] + 20, 0);
 
   return (
     <>
-      { !!reqData && 
-        <div className="portfolio-table">
-          <RenderTags tags={reqData[0]} />
-          <RenderBody body={reqData} />
+      {!!reqData.data &&
+        <div className="table">
+          <RenderTags tags={filterTags(reqData.data[0], correctName)} length={getLength} />
+          <RenderBody body={reqData.data} length={getLength} />
         </div>
       }
     </>
